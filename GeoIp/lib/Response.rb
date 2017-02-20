930de80749dc66d122e0887b5313b4faaa39535e
Yuri Delgado <ymdelgado@hotmail.com>
2017-02-20 13:45:26 -0600

    change sym to str fr hash keys

--- GeoIp/lib/Response.rb
+++ GeoIp/lib/Response.rb
@@ -1,4 +1,5 @@
 class Response
+
   attr_accessor :ip,:country_code, :country_name,
     :region_code, :region_name, :city,
     :zip_code, :time_zone, :latitude,
@@ -6,16 +7,16 @@ class Response
 
   def set(hash)
     @ip = hash[:ip]
-    @country_code = hash[:country_code]
-    @country_name = hash[:country_name]
-    @region_code  = hash[:region_code]
-    @region_name = hash[:region_name]
-    @city  = hash[:city]
-    @zip_code  = hash[:zip_code]
-    @time_zone  = hash[:time_zone]
-    @latitude  = hash[:latitude]
-    @longitude  = hash[:longitude]
-    @metro_code  = hash[:metro_code]
+    @country_code = hash["country_code"]
+    @country_name = hash["country_name"]
+    @region_code  = hash["region_code"]
+    @region_name = hash["region_name"]
+    @city  = hash["city"]
+    @zip_code  = hash["zip_code"]
+    @time_zone  = hash["time_zone"]
+    @latitude  = hash["latitude"]
+    @longitude  = hash["longitude"]
+    @metro_code  = hash["metro_code"]
   end
 
 end
