9390f484a7b7a695038d2a08b42ef5cc76ce6e63
Yuri Delgado <ydelgado@MacBook-Pro-de-Yuri.local>
2017-02-20 13:25:15 -0600

    Response class

---
+++ Response.rb
@@ -0,0 +1,21 @@
+class Response
+  attr_accessor :ip,:country_code, :country_name,
+    :region_code, :region_name, :city,
+    :zip_code, :time_zone, :latitude,
+    :longitude, :metro_code
+
+  def set(hash)
+    @ip = hash[:ip]
+    @country_code = hash[:country_code]
+    @country_name = hash[:country_name]
+    @region_code  = hash[:region_code]
+    @region_name = hash[:region_name]
+    @city  = hash[:city]
+    @zip_code  = hash[:zip_code]
+    @time_zone  = hash[:time_zone]
+    @latitude  = hash[:latitude]
+    @longitude  = hash[:longitude]
+    @metro_code  = hash[:metro_code]
+  end
+
+end
